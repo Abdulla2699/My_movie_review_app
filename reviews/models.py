ba71f76f9dc31dc122eb856e05b757123986b3c6
abdulla2699 <abdullamohamed99p@gmail.com>
2025-04-09 14:23:01 +0200

    updating the models.py to apply release_date

--- reviews/models.py
+++ reviews/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     description = models.TextField()
+    release_date = models.DateField()
 
     def __str__(self):
         return self.title
