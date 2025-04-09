ce9f1cd6b3646c24886f90fa6e02c79d92ce8b75
abdulla2699 <abdullamohamed99p@gmail.com>
2025-04-09 17:32:37 +0200

    updating views.py to add movie view set and review view set

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Movie, Review
 from .serializers import MovieSerializer, ReviewSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 class MovieListCreateView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
@@ -26,10 +28,16 @@ class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]  # Only authenticated
-from django_filters.rest_framework import DjangoFilterBackend
-
+    
 class MovieListCreateView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['title', 'release_date']
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
