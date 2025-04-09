e6d152c10e53cbc66c81e95ae26b84ef24e4d1b7
abdulla2699 <abdullamohamed99p@gmail.com>
2025-04-09 14:28:14 +0200

    testing

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Movie
+
+class MovieTests(APITestCase):
+    def test_create_movie(self):
+        data = {"title": "Test Movie", "description": "Just a test.", "release_date": "2025-01-01"}
+        response = self.client.post('/api/movies/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
