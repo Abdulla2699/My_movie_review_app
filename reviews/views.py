3f69206b57b6f38da16f4e0611a33902b5b9639b
abdulla2699 <abdullamohamed99p@gmail.com>
2025-04-09 12:59:52 +0200

    updated views.py

--- reviews/views.py
+++ reviews/views.py
@@ -11,3 +11,9 @@ class MovieListCreateView(generics.ListCreateAPIView):
 class ReviewListCreateView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+class ReviewListByMovieView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        movie_id = self.kwargs['movie_id']
+        return Review.objects.filter(movie_id=movie_id)
