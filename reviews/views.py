5a75bf986ee875b74494881f788a16ed5aceecf8
abdulla2699 <abdullamohamed99p@gmail.com>
2025-04-09 12:52:47 +0200

    Created views for Movie and Review endpoints

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Movie, Review
+from .serializers import MovieSerializer, ReviewSerializer
+
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
